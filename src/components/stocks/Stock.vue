0fa299bf72ff4dc1f3efab1d3a1683924ef01334
Unknown <riderjensen@gmail.com>
2018-11-12 10:39:19 -0700

    Fixing diabled button issue

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -9,7 +9,7 @@
 					<input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
 				</div>
 				<div class="pull-right">
-					<button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Buy</button>
+					<button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Buy</button>
 				</div>
 			</div>
 		</div>
