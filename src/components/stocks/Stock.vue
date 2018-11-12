a5431ad594f1471e910fa6638c09eee9f77b6d5c
Unknown <riderjensen@gmail.com>
2018-11-12 09:58:55 -0700

    adding basic routing

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Stock Stock Component</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Stock Stocks Component</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
