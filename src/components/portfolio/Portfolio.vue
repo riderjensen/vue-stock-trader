d6de25cae1835978ca3506ee64e48d82cab6cf79
Unknown <riderjensen@gmail.com>
2018-11-12 12:17:57 -0700

    Stock buying and selling working

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,9 +1,22 @@
 <template>
-	<h1>Portfolio</h1>
+	<div>
+		<app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+	</div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+import Stock from "./Stock.vue";
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: "stockPortfolio"
+    })
+  },
+  components: {
+    appStock: Stock
+  }
+};
 </script>
 
 <style scoped>
--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="col-sm-6 col-md-4">
-		<div class="panel panel-success">
+		<div class="panel panel-info">
 			<div class="panel-heading">
-				<h3 class="panel-title">{{ stock.name }}<small>(Price: {{ stock.price }})</small></h3>
+				<h3 class="panel-title">{{ stock.name }}<small>(Price: {{ stock.price }} | Quantity {{ stock.quantity }})</small></h3>
 			</div>
 			<div class="panel-body">
 				<div class="pull-left">
 					<input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
 				</div>
 				<div class="pull-right">
-					<button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Buy</button>
+					<button class="btn btn-success" @click="sellStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Sell</button>
 				</div>
 			</div>
 		</div>
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   props: ["stock"],
   data() {
@@ -25,13 +26,16 @@ export default {
     };
   },
   methods: {
-    buyStock() {
+    ...mapActions({
+      placeSellOrder: "sellStock"
+    }),
+    sellStock() {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
         quantity: this.quantity
       };
-      this.$store.dispatch("buyStock", order);
+      this.placeSellOrder(order);
       this.quantity = 0;
     }
   }
