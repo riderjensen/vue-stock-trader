3d5435285296742612d86262db79995684d02740
Unknown <riderjensen@gmail.com>
2018-11-12 10:38:19 -0700

    Adding stock styling

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,42 @@
 <template>
-	<h1>Stock Stocks Component</h1>
+	<div>
+		<app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+	</div>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+export default {
+  data() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: "BMW",
+          price: 110
+        },
+        {
+          id: 2,
+          name: "Google",
+          price: 200
+        },
+        {
+          id: 3,
+          name: "Apple",
+          price: 80
+        },
+        {
+          id: 4,
+          name: "Twitter",
+          price: 8
+        }
+      ]
+    };
+  },
+  components: {
+    appStock: Stock
+  }
+};
 </script>
 
 <style scoped>
